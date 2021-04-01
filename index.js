da240efad970f778e2a372691ad85884e9caa76c
grif <gmpaier@loyola.edu>
2021-03-31 23:01:49 -0400

    Working version. Styling is nothing to write home about but somehow bootstrap was giving me difficulties and its coming down to the last minute. No big deal. As Sajan says, perfection is the enemy of good.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const employeeList = [];
 
@@ -33,7 +35,7 @@ async function addManager() {
 
     }
     catch(err){
-        console.log("There seems to be an error")
+        console.log("Error in addManager")
     }
     
 }
@@ -51,20 +53,18 @@ async function addNew(){
 
     switch(response){
       case "Yes, add new Engineer":
-        addEngineer();
-        break;
+        return addEngineer();
+
       case "Yes, add new Intern":
-        addIntern();
-        break;
+        return addIntern();
+
       case "No, export to HTML":
-        postData();
-        break;
-      default:
-        throw err;
+        return postData();
+
     }
   }
   catch(err){
-    console.log("There seems to be an error")
+    console.log("Error in addNew")
   }
 }
 
@@ -94,7 +94,7 @@ async function addEngineer(){
 
   }
   catch(err){
-    console.log("There seems to be an error")
+    console.log("Error in addEngineer")
   }
 }
 
@@ -124,15 +124,92 @@ async function addIntern(){
 
   }
   catch(err){
-    console.log("There seems to be an error")
+    console.log("Error in addIntern")
   }
 }
 
-function postData(){
-  console.log("Success!\n");
-  for (let i = 0; i < employeeList.length; i++){
-    console.log(employeeList[i].getName() + "\n");
+async function postData(){
+try{
+let content = "";
+ for (let i = 0; i < employeeList.length; i++){
+  let role = employeeList[i].getRole();
+
+  switch(role){
+    case "Manager":
+      content += `<div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${employeeList[0].getName()}</h5>
+        <h5 class="card-title"><span class="fa fa-mug-hot"></span>Manager</h5>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${employeeList[0].getId()}</li>
+          <li class="list-group-item">Email: ${employeeList[0].getEmail()}</li>
+          <li class="list-group-item">Office number: ${employeeList[0].officeNumber}</li>
+        </ul>
+      </div>
+    </div>
+    `
+      break;
+    case "Intern":
+      content += `<div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${employeeList[i].getName()}</h5>
+        <h5 class="card-title"><span class="fa fa-user-graduate"></span>Intern</h5>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${employeeList[i].getId()}</li>
+          <li class="list-group-item">Email: ${employeeList[i].getEmail()}</li>
+          <li class="list-group-item">School: ${employeeList[i].getSchool()}</li>
+        </ul>
+      </div>
+    </div>
+  `
+      break;
+    case "Engineer":
+      content += `<div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${employeeList[i].getName()}</h5>
+        <h5 class="card-title"><span class="fa fa-glasses"></span>Engineer</h5>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${employeeList[i].getId()}</li>
+          <li class="list-group-item">Email: ${employeeList[i].getEmail()}</li>
+          <li class="list-group-item">Github: ${employeeList[i].getGithub()}</li>
+        </ul>
+      </div>
+    </div>
+  `   
+      break;
   }
+ }
+
+ const template = `<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Team Profile</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+     <link href="style.css" rel="stylesheet">
+ </head>
+ <body>
+   <header class="team-header">
+     <h1>My Team</h1>
+   </header>
+   <section id="main">
+   ${content}
+ </section>
+ </body>
+ </html>
+ ` 
+ await writeFileAsync("./dist/index.html", template, "utf8");
 }
+catch(err){
+  console.log("Error in postData")
+}
+}
+
+
+
 
 addManager();
+
